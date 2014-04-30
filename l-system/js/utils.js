465e33242283bd6b7f591f01b36dfd7eb17650f1
Ando Yasushi <andyjpn@gmail.com>
2014-04-30 23:53:15 +0900

    url encode

--- l-system/js/utils.js
+++ l-system/js/utils.js
@@ -2,7 +2,7 @@ var Utils = {
   getParams: function() {
     var params = {};
     if (window.location.search) {
-      var pairs = window.location.search.substring(1).split('&');
+      var pairs = window.location.search.substring(1).replace(/%5B/g, '[').replace(/%5D/g, ']').split('&');
       for (var i = 0; i < pairs.length; i++) {
         var pair = pairs[i].split('=');
         params[pair[0]] = pair[1];
