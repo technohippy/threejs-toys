a4a86e077f679304d658d1ef48bfcc4dcc683e4d
Ando Yasushi <andyjpn@gmail.com>
2014-06-25 13:56:07 +0900

    smartphone

--- angry-birds/js/angry-birds-start.js
+++ angry-birds/js/angry-birds-start.js
@@ -3,7 +3,21 @@ var AngryBirds = AngryBirds || {};
 (function(AngryBirds) {
 "use strict";
 
+function isSmartphone() {
+  var ua = navigator.userAgent;
+  var keys = ['iPhone', 'iPad', 'iPod', 'Android'];
+  for (var i = 0; i < keys.length; i++) {
+    if (0 <= ua.indexOf(keys[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
 window.addEventListener('load', function() {
+  if (isSmartphone()) {
+    window.location = '/controller.html';
+  }
   AngryBirds.Game.start({stages:[
     new AngryBirds.Stage1(),
     new AngryBirds.Stage2(),
