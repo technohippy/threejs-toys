f27f3a6b591579f3ef99ee893199b710f696eaeb
Ando Yasushi <andyjpn@gmail.com>
2014-07-04 00:19:41 +0900

    stage4

--- angry-birds/js/angry-birds-stage4.js
+++ angry-birds/js/angry-birds-stage4.js
@@ -46,13 +46,25 @@ AngryBirds.Stage4.prototype.constructOn = function(world) {
   this.piggies.push(piggy1);
 
   world.add(this.createBoard(
+    {width:0.5, height:2, depth:10}, 
+    {x:this.denPosition.x-4, y:20, z:this.denPosition.z}, 
+    {fixed:true, map:AngryBirds.Texture.CLOD, bumpMap:AngryBirds.Texture.CLOD}
+  ));
+
+  world.add(this.createBoard(
+    {width:0.5, height:2, depth:10}, 
+    {x:this.denPosition.x+4, y:20, z:this.denPosition.z}, 
+    {fixed:true, map:AngryBirds.Texture.CLOD, bumpMap:AngryBirds.Texture.CLOD}
+  ));
+
+  world.add(this.createBoard(
     {width:10, height:1, depth:10}, 
     {x:this.denPosition.x, y:19, z:this.denPosition.z}, 
     {fixed:true, map:AngryBirds.Texture.CLOD, bumpMap:AngryBirds.Texture.CLOD}
   ));
 
-  for (var dx = -4; dx <= 4; dx += 8) {
-    for (var dz = -4; dz <= 4; dz += 8) {
+  for (dx = -4; dx <= 4; dx += 8) {
+    for (dz = -4; dz <= 4; dz += 8) {
       world.add(this.createBoard(
         {width:1, height:20, depth:1}, 
         {x:this.denPosition.x+dx, y:20/2-1, z:this.denPosition.z+dz}, 
@@ -61,10 +73,14 @@ AngryBirds.Stage4.prototype.constructOn = function(world) {
     }
   }
 
-  var piggy2 = new AngryBirds.Piggy(0.6, {mass:0.5, angularDamping:0.8, ambient:0x999999});
-  piggy2.position.set(this.denPosition.x, 19+1/2+0.6/2, this.denPosition.z);
-  world.add(piggy2);
-  this.piggies.push(piggy2);
+  for (dx = -1.5; dx <= 1.5; dx += 3) {
+    for (dz = -1.5; dz <= 1.5; dz += 3) {
+      var piggy = new AngryBirds.Piggy(0.6, {mass:0.5, angularDamping:0.8, ambient:0x999999});
+      piggy.position.set(this.denPosition.x+dx, 19+1/2+0.6/2, this.denPosition.z+dz);
+      world.add(piggy);
+      this.piggies.push(piggy);
+    }
+  }
 
 };
 
